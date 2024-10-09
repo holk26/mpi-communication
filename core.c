cf41a9e53c9c05092a20d8906e27d962d4eed959
Homero Cabrera Araque <homero9726@gmail.com>
2024-10-08 20:49:56 -0700

    Update core.c

--- core.c
+++ core.c
@@ -1,17 +1,11 @@
-#include <mpi.h>
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    MPI_Init(NULL, NULL);  // Inicializar el entorno MPI
-
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);  // Obtener número total de procesos
-
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);  // Obtener el rango del proceso actual
-
-    printf("Hola mundo desde el procesador %d de %d procesadores\n", world_rank, world_size);
-
-    MPI_Finalize();  // Finalizar el entorno MPI
+#include "mpi.h"
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf("Soy el core nro. %d de %d\n", rank, size);
+    MPI_Finalize();
     return 0;
 }
